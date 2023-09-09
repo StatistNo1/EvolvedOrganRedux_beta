daaf6d3d78ab7d15af676c2c2813fb3ad4542881
StatistNo1 <80486109+StatistNo1@users.noreply.github.com>
2023-09-09 08:24:36 +0200

    Initial commit

---
+++ 1.4/Source/EvolvedOrgansRedux/EvolvedOrgansRedux/Harmony_Patches.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+
+namespace EvolvedOrgansRedux {
+    [Verse.StaticConstructorOnStartup]
+    public static class HarmonyPatches {
+        private static readonly System.Type patchType = typeof(HarmonyPatches);
+        private static HarmonyLib.Harmony harmony = null;
+
+        static HarmonyPatches() {
+            try {
+                harmony = new HarmonyLib.Harmony("EvolvedOrgansRedux");
+                Singleton.Instance.NameOfThisMod = harmony.Id;
+                Patch();
+            } catch (System.Exception e) {
+                string errorMessage = "EvolvedOrgansRedux -> Error: Step one";
+                errorMessage += "\n" + e;
+                Verse.Log.Error(errorMessage);
+            }
+        }
+        private static void Patch() {
+            PatcherPostFix(typeof(Verse.PawnCapacityUtility), nameof(Verse.PawnCapacityUtility.CalculateLimbEfficiency), nameof(CalculateLimbEfficiency_PostFix));
+            PatcherPostFix(typeof(RimWorld.AgeInjuryUtility), nameof(RimWorld.AgeInjuryUtility.RandomHediffsToGainOnBirthday), nameof(RandomHediffsToGainOnBirthday_Postfix), new System.Type[] { typeof(Verse.Pawn), typeof(int) });
+            PatcherPostFix(typeof(RimWorld.Recipe_RemoveBodyPart), nameof(RimWorld.Recipe_RemoveBodyPart.GetPartsToApplyOn), nameof(Recipe_RemoveBodyPart_Postfix));
+        }
+        private static void CalculateLimbEfficiency_PostFix(Verse.BodyPartTagDef limbCoreTag, ref float __result) {
+            foreach (System.Tuple<Verse.BodyPartTagDef, float> t in Singleton.Instance.BodyPartTagsToRecalculate) {
+                if (limbCoreTag == t.Item1) {
+                    __result = ((__result - 1f) * t.Item2) + 1f;
+                }
+            }
+        }
+        private static void RandomHediffsToGainOnBirthday_Postfix(Verse.Pawn pawn, ref System.Collections.Generic.IEnumerable<Verse.HediffGiver_Birthday> __result) {
+            if (pawn.health.hediffSet.HasHediff(Verse.HediffDef.Named("EVOR_Hediff_AbdominalAndChestcavity_RasVacoule"))) {
+                if (Verse.Prefs.LogVerbose)
+                    Verse.Log.Message("EvolvedOrgansRedux -> Pawn is immortal because of Ras Vacoule.");
+                __result = System.Linq.Enumerable.Empty<Verse.HediffGiver_Birthday>();
+            }
+        }
+        private static void Recipe_RemoveBodyPart_Postfix(ref System.Collections.Generic.IEnumerable<Verse.BodyPartRecord> __result) {
+            //Remove the Recipe_RemoveBodyPart for the bodyparts added by this mod.
+            //I would rather use my custom Recipe_RemoveImplant.
+            __result = __result.Where(bodypartrecord => bodypartrecord.def != DefOf.LowerShoulder && bodypartrecord.def != DefOf.Back &&
+                bodypartrecord.def != DefOf.BodyCavity1 && bodypartrecord.def != DefOf.BodyCavity2 && bodypartrecord.def != DefOf.BodyCavityA
+                && !bodypartrecord.def.defName.ToLower().Contains("tail"));
+        }
+        private static void PatcherPostFix(System.Type typeOfMethodToPatch, string nameOfMethodToPatch, string nameOfPatcherMethod, System.Type[] parameters = null) {
+            if (Verse.Prefs.LogVerbose)
+                Verse.Log.Message("EvolvedOrgansRedux -> Currently patching: " + nameOfPatcherMethod);
+            harmony.Patch(HarmonyLib.AccessTools.Method(type: typeOfMethodToPatch, name: nameOfMethodToPatch, parameters),
+                      postfix: new HarmonyLib.HarmonyMethod(methodType: patchType, methodName: nameOfPatcherMethod));
+        }
+        //private static void PatcherTranspiler(System.Type typeOfMethodToPatch, string nameOfMethodToPatch, string nameOfPatcherMethod, System.Type[] parameters = null) {
+        //    if (Verse.Prefs.LogVerbose)
+        //        Verse.Log.Message("EvolvedOrgansRedux -> Currently patching: " + nameOfPatcherMethod);
+        //    harmony.Patch(HarmonyLib.AccessTools.Method(type: typeOfMethodToPatch, name: nameOfMethodToPatch, parameters),
+        //              transpiler: new HarmonyLib.HarmonyMethod(methodType: patchType, methodName: nameOfPatcherMethod));
+        //}
+
+        //static IEnumerable<CodeInstruction> CalculateCapacityLevel_Transpilerfix(IEnumerable<CodeInstruction> instructions) {
+        //    bool instructionFound = false;
+        //    MethodInfo callToMathfMax = AccessTools.Method(typeof(Mathf), nameof(Mathf.Max), parameters: new System.Type[] { typeof(float), typeof(float) });
+        //    foreach (CodeInstruction instruction in instructions) {
+        //        yield return instruction;
+        //        if (instruction.Calls(callToMathfMax)) {
+        //            yield return new CodeInstruction(OpCodes.Ldarg_1);
+        //            yield return CodeInstruction.Call(patchType, nameof(FixCapacity));
+        //            instructionFound = true;
+        //        }
+        //    }
+        //    if (!instructionFound)
+        //        Log.Error("EvolvedOrgansRedux -> Instruction not found in CalculateCapacityLevel_Transpilerfix");
+        //
+        //}
+        //public static float FixCapacity(float capacityValue, BodyPartTagDef limbCoreTag) {
+        //    foreach (System.Tuple<Verse.BodyPartTagDef, float> t in Singleton.Instance.BodyPartTagsToRecalculate) {
+        //        if (limbCoreTag == t.Item1) {
+        //            capacityValue = ((capacityValue - 1f) * t.Item2) + 1f;
+        //        }
+        //    }
+        //    return capacityValue;
+        //}
+    }
+}
---
+++ 1.4/Source/EvolvedOrgansRedux/EvolvedOrgansRedux/Settings.cs
@@ -0,0 +1,55 @@
+﻿namespace EvolvedOrgansRedux {
+    public class Settings : Verse.ModSettings {
+        public bool BodyPartAffinity;
+        public string ChosenWorkbench;
+        public bool ImportantMessage20320905;
+        public int AmountOfArms;
+        public System.Collections.Generic.List<string> ChoicesOfWorkbenches = new() { "Evolved Organs Redux" };
+        //public System.Collections.Generic.List<int> ChoicesForAmountOfArms = new (){ 2, 4, 6, 8 };
+        public override void ExposeData() {
+            Verse.Scribe_Values.Look(ref BodyPartAffinity, "BodyPartAffinity");
+            Verse.Scribe_Values.Look(ref ChosenWorkbench, "ChosenWorkbench");
+            Verse.Scribe_Values.Look(ref ImportantMessage20320905, "RemovedSetting-CombatibilitySwitchEORVersionMidSave", false, true);
+            //Verse.Scribe_Values.Look(ref AmountOfArms, "AmountOfArms", defaultValue: 4);
+            base.ExposeData();
+        }
+        public Settings() {
+            if (ChoicesOfWorkbenches.Contains(ChosenWorkbench))
+                ChosenWorkbench = ChoicesOfWorkbenches[0];
+        }
+    }
+
+    public class EvolvedOrgansReduxSettings : Verse.Mod {
+        readonly Settings settings;
+        public EvolvedOrgansReduxSettings(Verse.ModContentPack content) : base(content) {
+            this.settings = GetSettings<Settings>();
+        }
+        public override void DoSettingsWindowContents(UnityEngine.Rect inRect) {
+            Verse.Listing_Standard listingStandard = new Verse.Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Label("Check this option if you want to put archotech arms on lower shoulders or additional non-Evor lungs into your body.\nThe game has to be restarted for this change to take effect.");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("BodyPartAffinity", ref settings.BodyPartAffinity);
+            listingStandard.GapLine();
+            listingStandard.Label("Check of which mod the organ workbench should be used to create EvolvedOrgansRedux implants.\nThe game has to be restarted for this change to take effect.");
+            listingStandard.Gap();
+            for (int i = 0; i < settings.ChoicesOfWorkbenches.Count; i++) {
+                if (listingStandard.RadioButton(settings.ChoicesOfWorkbenches[i], settings.ChosenWorkbench == settings.ChoicesOfWorkbenches[i], tabIn: 30f)) {
+                    settings.ChosenWorkbench = settings.ChoicesOfWorkbenches[i];
+                }
+            }
+            //listingStandard.GapLine();
+            //listingStandard.Label("Choose up to how many arms your pawns should have.");
+            //listingStandard.Gap();
+            //for (int i = 0; i < settings.ChoicesForAmountOfArms.Count; i++) {
+            //    if (listingStandard.RadioButton(settings.ChoicesForAmountOfArms[i].ToString(), settings.AmountOfArms == settings.ChoicesForAmountOfArms[i], tabIn: 30f)) {
+            //        settings.AmountOfArms = settings.ChoicesForAmountOfArms[i];
+            //    }
+            //}
+            listingStandard.End();
+        }
+        public override string SettingsCategory() {
+            return "EvolvedOrgansRedux";
+        }
+    }
+}
